5a080f9f496e02809a3ac6911fe528125d2e0712
Wendy <weeluthuli@gmail.com>
2018-08-09 02:39:42 +0200

    Upload Photo

--- app/src/main/java/com/example/s215087038/wefixx/UploadImage.java
+++ app/src/main/java/com/example/s215087038/wefixx/UploadImage.java
@@ -1,18 +1,27 @@
 package com.example.s215087038.wefixx;
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -22,18 +31,30 @@ import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.s215087038.wefixx.EndPoints;
 import com.example.s215087038.wefixx.R;
 import com.example.s215087038.wefixx.VolleyMultipartRequest;
+import com.example.s215087038.wefixx.student.Student;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
+
+import static java.net.Proxy.Type.HTTP;
 
 
 public class UploadImage extends AppCompatActivity {
@@ -43,10 +64,12 @@ public class UploadImage extends AppCompatActivity {
     Matrix matrix;
     int newWidth, newHeight, width, height;
     float scaleWidth, scaleHeight;
-    Bitmap resizedImage;
+    Bitmap scaledBitmap = null, bitmap = null;
     ByteArrayOutputStream outputStream;
     ByteArrayOutputStream bytearrayoutputstream;
     byte[] BYTE;
+    String uploadUrl = "http://sict-iis.nmmu.ac.za/wefixx/student/new.php";
+    AlertDialog.Builder builder;
 
 
     //edittext for getting the tags input
@@ -59,7 +82,6 @@ public class UploadImage extends AppCompatActivity {
 
         //initializing views
         imageView = (ImageView) findViewById(R.id.ib_photo);
-        resizedView = (ImageView) findViewById(R.id.ib_resized);
 
         //checking the permission
         //if the permission is not given we will open setting to add permission
@@ -108,43 +130,284 @@ public class UploadImage extends AppCompatActivity {
 
 
 
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
-                width = bitmap.getWidth();
-                height = bitmap.getHeight();
-
-                scaleWidth = ((float) newWidth) / width;
-
-                scaleHeight = ((float) newHeight) / height;
-
-                matrix.postScale(scaleWidth, scaleHeight);
-
-                matrix.postRotate(-45);
-
-                bitmap.compress(Bitmap.CompressFormat.JPEG,100,bytearrayoutputstream);
-
-                BYTE = bytearrayoutputstream.toByteArray();
-
-                resizedImage = BitmapFactory.decodeByteArray(BYTE,0,BYTE.length);
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+               // imageView.setImageBitmap(bitmap);
+                compressImage(imageUri);
 
 
-                //displaying selected image to imageview
-                imageView.setImageBitmap(bitmap);
-
-
-                outputStream = new ByteArrayOutputStream();
-
-                resizedImage.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
-                resizedView.setImageBitmap(resizedImage);
-
-
-                //calling the method uploadBitmap to upload image
-                uploadBitmap(bitmap);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
+    public String compressImage(Uri imageUri) {
+
+        String filePath = getRealPathFromURI(imageUri.toString());
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+//      by setting this field as true, the actual bitmap pixels are not loaded in the memory. Just the bounds are loaded. If
+//      you try the use the bitmap here, you will get null.
+        options.inJustDecodeBounds = true;
+        Bitmap bmp = BitmapFactory.decodeFile(filePath, options);
+
+        int actualHeight = options.outHeight;
+        int actualWidth = options.outWidth;
+
+//      max Height and width values of the compressed image is taken as 816x612
+
+        float maxHeight = 816.0f;
+        float maxWidth = 612.0f;
+        float imgRatio = actualWidth / actualHeight;
+        float maxRatio = maxWidth / maxHeight;
+
+//      width and height values are set maintaining the aspect ratio of the image
+
+        if (actualHeight > maxHeight || actualWidth > maxWidth) {
+            if (imgRatio < maxRatio) {
+                imgRatio = maxHeight / actualHeight;
+                actualWidth = (int) (imgRatio * actualWidth);
+                actualHeight = (int) maxHeight;
+            } else if (imgRatio > maxRatio) {
+                imgRatio = maxWidth / actualWidth;
+                actualHeight = (int) (imgRatio * actualHeight);
+                actualWidth = (int) maxWidth;
+            } else {
+                actualHeight = (int) maxHeight;
+                actualWidth = (int) maxWidth;
+
+            }
+        }
+
+//      setting inSampleSize value allows to load a scaled down version of the original image
+
+        options.inSampleSize = calculateInSampleSize(options, actualWidth, actualHeight);
+
+//      inJustDecodeBounds set to false to load the actual bitmap
+        options.inJustDecodeBounds = false;
+
+//      this options allow android to claim the bitmap memory if it runs low on memory
+        options.inPurgeable = true;
+        options.inInputShareable = true;
+        options.inTempStorage = new byte[16 * 1024];
+
+        try {
+//          load the bitmap from its path
+            bmp = BitmapFactory.decodeFile(filePath, options);
+        } catch (OutOfMemoryError exception) {
+            exception.printStackTrace();
+
+        }
+        try {
+            scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.ARGB_8888);
+        } catch (OutOfMemoryError exception) {
+            exception.printStackTrace();
+        }
+
+        float ratioX = actualWidth / (float) options.outWidth;
+        float ratioY = actualHeight / (float) options.outHeight;
+        float middleX = actualWidth / 2.0f;
+        float middleY = actualHeight / 2.0f;
+
+        Matrix scaleMatrix = new Matrix();
+        scaleMatrix.setScale(ratioX, ratioY, middleX, middleY);
+
+        Canvas canvas = new Canvas(scaledBitmap);
+        canvas.setMatrix(scaleMatrix);
+        canvas.drawBitmap(bmp, middleX - bmp.getWidth() / 2, middleY - bmp.getHeight() / 2, new Paint(Paint.FILTER_BITMAP_FLAG));
+
+//      check the rotation of the image and display it properly
+        ExifInterface exif;
+        try {
+            exif = new ExifInterface(filePath);
+
+            int orientation = exif.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION, 0);
+            Log.d("EXIF", "Exif: " + orientation);
+            Matrix matrix = new Matrix();
+            if (orientation == 6) {
+                matrix.postRotate(90);
+                Log.d("EXIF", "Exif: " + orientation);
+            } else if (orientation == 3) {
+                matrix.postRotate(180);
+                Log.d("EXIF", "Exif: " + orientation);
+            } else if (orientation == 8) {
+                matrix.postRotate(270);
+                Log.d("EXIF", "Exif: " + orientation);
+            }
+            scaledBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0,
+                    scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix,
+                    true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FileOutputStream out = null;
+        String filename = getFilename();
+        try {
+            out = new FileOutputStream(filename);
+
+//          write the compressed bitmap at the destination specified by filename.
+            scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 80, out);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        imageView.setImageBitmap(scaledBitmap);
+        uploadBitmap(scaledBitmap);
+        return filename;
+
+    }
+
+    private void uploadBitmap(final Bitmap scaledBitmap) {
+
+        //getting the tag from the edittext
+
+        //our custom volley request
+        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, uploadUrl,
+                new Response.Listener<NetworkResponse>() {
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+
+                        try {
+
+
+                            JSONArray jsonArray = new JSONArray(new String (response.data));
+                            JSONObject jsonObject = jsonArray.getJSONObject(0);
+//                            String code = jsonObject.getString("code");
+                            String message = jsonObject.getString("message");
+
+//                            JSONObject jo = new JSONObject(new String(response.data));
+//
+//                            String message = jo.getString("message");
+
+                            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }) {
+
+            /*
+             * If you want to add more parameters with the image
+             * you can do it here
+             * here we have only one parameter with the image
+             * which is tags
+             * */
+
+
+            /*
+             * Here we are passing image by renaming it with a unique name
+             * */
+            @Override
+            protected Map<String, DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+                String imagename = "me";
+                params.put("pic", new DataPart(imagename + ".jpeg", getFileDataFromDrawable(bitmap)));
+                return params;
+            }
+        };
+
+        //adding the request to volley
+        Volley.newRequestQueue(this).add(volleyMultipartRequest);
+
+//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//        byte[] imageBytes = baos.toByteArray();
+//        final String imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+//
+//        StringRequest stringRequest = new StringRequest(Request.Method.POST, uploadUrl,
+//                    new Response.Listener<String>() {
+//                        @Override
+//                        public void onResponse(String response) {
+//                            try {
+//                                JSONArray jsonArray = new JSONArray(response);
+//                                JSONObject jsonObject = jsonArray.getJSONObject(0);
+//                                String code = jsonObject.getString("code");
+//                                String message = jsonObject.getString("message");
+//
+////                                if(code.equals("req_success")){
+////                                    String passengerID = jsonObject.getString("user_id");
+////                                    String passengerName = jsonObject.getString("name");
+////                                    createSessions(passengerID, passengerName);
+////                                }
+//                                Toast.makeText(UploadImage.this, message, Toast.LENGTH_LONG).show();
+//
+//                            } catch (JSONException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+//                    }, new Response.ErrorListener() {
+//                @Override
+//                public void onErrorResponse(VolleyError error) {
+//
+//                }
+//            }) {
+//                @Override
+//                protected Map<String, String> getParams() throws AuthFailureError {
+//                    Map<String, String> params = new HashMap<String, String>();
+//
+//                    params.put("photo", imageString);
+//
+//                    return params;
+//                }
+//
+//
+//            };
+//            MySingleton.getInstance(UploadImage.this).addToRequestque(stringRequest);
+
+
+    }
+
+    public String getFilename() {
+        File file = new File(Environment.getExternalStorageDirectory().getPath(), "MyFolder/Images");
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        String uriSting = (file.getAbsolutePath() + "/" + System.currentTimeMillis() + ".jpg");
+        return uriSting;
+
+    }
+
+    private String getRealPathFromURI(String contentURI) {
+        Uri contentUri = Uri.parse(contentURI);
+        Cursor cursor = getContentResolver().query(contentUri, null, null, null, null);
+        if (cursor == null) {
+            return contentUri.getPath();
+        } else {
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            return cursor.getString(index);
+        }
+    }
+
+    public int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+        final float totalPixels = width * height;
+        final float totalReqPixelsCap = reqWidth * reqHeight * 2;
+        while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+            inSampleSize++;
+        }
+
+        return inSampleSize;
+    }
     /*
      * The method is taking Bitmap as an argument
      * then it will return the byte[] array for the given bitmap
@@ -160,57 +423,4 @@ public class UploadImage extends AppCompatActivity {
         return byteArrayOutputStream.toByteArray();
     }
 
-    private void uploadBitmap(final Bitmap bitmap) {
-
-        //getting the tag from the edittext
-//        final String tags = editTextTags.getText().toString().trim();
-
-        //our custom volley request
-        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, EndPoints.UPLOAD_URL,
-                new Response.Listener<NetworkResponse>() {
-                    @Override
-                    public void onResponse(NetworkResponse response) {
-                        try {
-                            JSONObject obj = new JSONObject(new String(response.data));
-                            Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                }) {
-
-            /*
-             * If you want to add more parameters with the image
-             * you can do it here
-             * here we have only one parameter with the image
-             * which is tags
-             * */
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
-                params.put("tags", "mm");
-                return params;
-            }
-
-            /*
-             * Here we are passing image by renaming it with a unique name
-             * */
-            @Override
-            protected Map<String, DataPart> getByteData() {
-                Map<String, DataPart> params = new HashMap<>();
-                long imagename = System.currentTimeMillis();
-                params.put("pic", new DataPart(imagename + ".png", getFileDataFromDrawable(bitmap)));
-                return params;
-            }
-        };
-
-        //adding the request to volley
-        Volley.newRequestQueue(this).add(volleyMultipartRequest);
-    }
 }
